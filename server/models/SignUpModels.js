b369989a6cee557332acff6c4b69b604c2021360
Georgi Kalkovski <54138918+Georgi-Kalkovski@users.noreply.github.com>
2021-11-08 15:45:42 +0200

    Making some minor changes

--- server/models/SignUpModels.js
+++ server/models/SignUpModels.js
@@ -1,18 +1,21 @@
 const mongoose = require('mongoose');
 
 const signUpTemplate = new mongoose.Schema({
-    email:{
-        type:String,
-        required:true
+    email: {
+        type: String,
+        required: true
     },
-    password:{
-        type:String,
-        required:true
+    password: {
+        type: String,
+        required: true
     },
-    books:[{bookName:String, page:Number}],
-    date:{
-        type:Date,
-        default:Date.now
+    books: [{
+        bookName: { type: String, required: true },
+        page: { type: Number, required: true },
+    }],
+    date: {
+        type: Date,
+        default: Date.now
     },
 });
 
--- server/routes/routes.js
+++ server/routes/routes.js
@@ -3,18 +3,18 @@ const router = express.Router();
 const signUpTemplateCopy = require('../models/SignUpModels');
 
 router.post('/signup', (request, response) => {
-    console.log(request.body)
     const signedUpUser = new signUpTemplateCopy({
-        email:request.body.email,
-        password:request.body.password
+        email: request.body.email,
+        password: request.body.password,
+        books: request.body.books
     });
     signedUpUser.save()
-    .then(data => {
-        response.json(data)
-    })
-    .catch(error => {
-        response.json(error)
-    })
+        .then(data => {
+            response.json(data)
+        })
+        .catch(error => {
+            response.json(error)
+        })
 });
 
 module.exports = router;

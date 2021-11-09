b682e623361a9de1df804fa97d083bbc2c251025
Georgi Kalkovski <54138918+Georgi-Kalkovski@users.noreply.github.com>
2021-11-09 13:59:46 +0200

    Moving project to old-project folder... time to start fresh!

--- old-project/client/src/components/Signup.js
+++ old-project/client/src/components/Signup.js
@@ -1,36 +1,52 @@
 import React, { useState } from 'react';
+const axios = require('axios');
+const FormData = require('form-data');
 
 export default function Signup() {
     const [email, setEmail] = useState("");
     const [password, setPassword] = useState("");
 
     const submitChangeHanfler = (e) => {
-        console.log(e.target[0]);
-        console.log(e.target[1]);
-        setEmail(e.target[0]);
-        setPassword(e.target[1]);
+        e.preventDefault();
+        console.log(e.target[0].value);
+        console.log(e.target[1].value);
+        setEmail(e.target[0].value);
+        setPassword(e.target[1].value);
     };
+
+    
+    async function makeGetRequest() {
+        const form_data = new FormData();
+        form_data.append('email', email);
+        form_data.append('password', password);
+    
+        axios.post('http://localhost:4000/app/signup')
+        .then(response => console.log(response.form_data));
+    }
+    
+    makeGetRequest();
+
     return (
         <>
             <div className="container">
                 <div className="form-div">
-                    <form>
+                    <form onSubmit={submitChangeHanfler} >
                         <input type="text"
                             placeholder="E-mail"
                             className="form-control form-group"
-                            onChange={setEmail}
+                            onChange={e => setEmail(e.target.value)}
                             value={email} />
 
                         <input type="password"
                             placeholder="Password"
                             className="form-control form-group"
-                            onChange={setPassword}
+                            onChange={e => setPassword(e.target.value)}
                             value={password} />
 
                         <input type="submit"
                             className="btn btn-danger btn-block"
                             value="Submit"
-                            onClick={submitChangeHanfler} />
+                        />
                     </form>
                 </div>
             </div>

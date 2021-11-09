f8f4899790275ad2da850b047b5800c711a1190b
Georgi Kalkovski <54138918+Georgi-Kalkovski@users.noreply.github.com>
2021-11-09 13:00:39 +0200

    Update Signup.js

--- client/src/components/Signup.js
+++ client/src/components/Signup.js
@@ -1,51 +1,39 @@
-import React, { Component } from 'react';
-import "bootstrap/dist/css/bootstrap.min.css";
+import React, { useState } from 'react';
 
-export default class Signup extends Component {
-    constructor() {
-        super();
-        this.state = {
-            email: '',
-            password: ''
-        };
+export default function Signup() {
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+
+    const submitChangeHanfler = (e) => {
+        console.log(e.target[0]);
+        console.log(e.target[1]);
+        setEmail(e.target[0]);
+        setPassword(e.target[1]);
     };
+    return (
+        <>
+            <div className="container">
+                <div className="form-div">
+                    <form>
+                        <input type="text"
+                            placeholder="E-mail"
+                            className="form-control form-group"
+                            onChange={setEmail}
+                            value={email} />
 
-    chaneEmail(event) {
-        this.setState({
-            email: event.target.value
-        });
-    };
-    chanePassword(event) {
-        this.setState({
-            password: event.target.value
-        });
-    };
+                        <input type="password"
+                            placeholder="Password"
+                            className="form-control form-group"
+                            onChange={setPassword}
+                            value={password} />
 
-    render() {
-        return (
-            <>
-                <div className="container">
-                    <div className="form-div">
-                        <form>
-                            <input type="text"
-                                placeholder="E-mail"
-                                onChange={this.changeEmail}
-                                value={this.state.email}
-                                className="form-control form-group" />
-
-                            <input type="password"
-                                placeholder="Password"
-                                onChange={this.changePassword}
-                                value={this.state.password}
-                                className="form-control form-group" />
-
-                            <input type="submit"
-                                className="btn btn-danger btn-block"
-                                value="Submit" />
-                        </form>
-                    </div>
+                        <input type="submit"
+                            className="btn btn-danger btn-block"
+                            value="Submit"
+                            onClick={submitChangeHanfler} />
+                    </form>
                 </div>
-            </>
-        );
-    };
+            </div>
+        </>
+    );
 };

6cf561f753c11f9d25a3ecffd59a31f55bdc3e5d
Georgi Kalkovski <54138918+Georgi-Kalkovski@users.noreply.github.com>
2021-11-10 15:56:39 +0200

    Adding frontend to the project

---
+++ react-jwt-auth/src/components/BoardModerator.js
@@ -0,0 +1,35 @@
+import React, { useState, useEffect } from 'react';
+
+import ModeratorService from '../services/user.service';
+
+const BoardModerator = () => {
+  const [content, setContent] = useState('');
+
+  useEffect(() => {
+    ModeratorService.getModeratorBoard().then(
+      (response) => {
+        setContent(response.data);
+      },
+      (error) => {
+        const _content =
+          (error.response &&
+            error.response.data &&
+            error.response.data.message) ||
+          error.message ||
+          error.toString();
+
+        setContent(_content);
+      }
+    );
+  }, []);
+
+  return (
+    <div className='container'>
+      <header className='jumbotron'>
+        <h3>{content}</h3>
+      </header>
+    </div>
+  );
+};
+
+export default BoardModerator;

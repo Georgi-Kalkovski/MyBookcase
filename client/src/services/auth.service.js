822b629b5a20f304aac289aa665387a1fe17e937
Georgi Kalkovski <54138918+Georgi-Kalkovski@users.noreply.github.com>
2021-11-15 13:54:38 +0200

    Fixing code

--- client/src/services/auth.service.js
+++ client/src/services/auth.service.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API_URL = process.env.API_URL + "/api/test/";
+const API_URL = process.env.REACT_APP_API_URL;
 
 const register = (username, email, password) => {
   return axios.post(API_URL + "signup", {
--- client/src/services/user.service.js
+++ client/src/services/user.service.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 import authHeader from "./auth-header";
 
-const API_URL = process.env.API_URL + "/api/test/";
+const API_URL = process.env.REACT_APP_API_URL;
 
 const getPublicContent = () => {
   return axios.get(API_URL + "all");

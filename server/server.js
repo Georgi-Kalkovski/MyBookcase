96067dddc15a60a84132d2f5ee5d428ae15e9d1e
Georgi Kalkovski <54138918+Georgi-Kalkovski@users.noreply.github.com>
2021-11-15 14:29:32 +0200

    Adding .env file

--- server/server.js
+++ server/server.js
@@ -5,7 +5,7 @@ const cors = require("cors");
 const app = express();
 
 var corsOptions = {
-  origin: "http://localhost:3000"
+  origin: ["http://localhost:3000","https://thebookcase.herokuapp.com"]
 };
 
 app.use(cors(corsOptions));

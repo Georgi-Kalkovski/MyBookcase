b67517d3adffcbfc9f8b45db446c61760b3593fd
Georgi Kalkovski <54138918+Georgi-Kalkovski@users.noreply.github.com>
2021-12-13 16:03:23 +0200

    Working on ArrowUp function

--- client/src/components/Footer.js
+++ client/src/components/Footer.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useRef, useEffect } from 'react';
 import './Footer.css';
 import github from './img/github.svg';
 import linkedin from './img/linkedin.svg';
@@ -6,30 +6,46 @@ import gmail from './img/gmail.svg';
 import facebook from './img/facebook.svg';
 import discord from './img/discord.svg';
 
-const Footer = () => (
-    <div className='footer'>
-        <footer id="footer" className="footer text-center">
+const Footer = () => {
+    const arrow = useRef(null);
 
-            <nav id="footerNav">
-                <div className="container">
-                    <ul>
-                        <p style={{color:'#ad9b80', marginBottom:'-5px', fontSize: '10px'}}>Made with much ❤️ by Georgi Kalkovski aka Terter</p>
-                        <a className="contactMe nav-link" href="https://github.com/Georgi-Kalkovski"><img src={github} /></a>
-                        <a className="contactMe nav-link" href="https://www.linkedin.com/in/georgi-kalkovski/"><img src={linkedin} /></a>
-                        <a className="contactMe nav-link" href="mailto:g.kalkovski.92@gmail.com"><img src={gmail} /></a>
-                        <a className="contactMe nav-link" href="https://www.facebook.com/georgi.kalkovski"><img src={facebook} /></a>
-                        <a className="contactMe nav-link" href="https://discord.com/users/242250226545590274"><img src={discord} /></a>
-                    </ul>
+    const myScrollFunc = () => (() => {
+        var y = arrow.current.focus();
+        if (y >= 100) {
+            arrow.current.style.display = 'block';
+        } else {
+            arrow.current.style.display = 'none';
+        }
+    });
+
+    return (
+        <div className='footer'>
+            <footer id='footer' className='footer text-center'>
+
+                <nav id='footerNav'>
+                    <div className='container'>
+                        <ul>
+                            <p style={{ color: '#ad9b80', marginBottom: '-5px', fontSize: '10px' }}>Made with much ❤️ by Georgi Kalkovski aka Terter</p>
+                            <a className='contactMe nav-link' href='https://github.com/Georgi-Kalkovski'><img src={github} /></a>
+                            <a className='contactMe nav-link' href='https://www.linkedin.com/in/georgi-kalkovski/'><img src={linkedin} /></a>
+                            <a className='contactMe nav-link' href='mailto:g.kalkovski.92@gmail.com'><img src={gmail} /></a>
+                            <a className='contactMe nav-link' href='https://www.facebook.com/georgi.kalkovski'><img src={facebook} /></a>
+                            <a className='contactMe nav-link' href='https://discord.com/users/242250226545590274'><img src={discord} /></a>
+                        </ul>
+                    </div>
+                </nav>
+
+                <div id='copyright'>
+                    <a href='https://github.com/Georgi-Kalkovski/MyBookcase'>&copy; MyBookcase - {new Date().getFullYear()}</a>
                 </div>
-            </nav>
 
-            <div id="copyright">
-                &copy; MyBookcase - {new Date().getFullYear()}
-            </div>
+                <div ref={arrow} onScroll={myScrollFunc}>
+                    <a href='#' className='arrowUp'><h3>⮝</h3></a>
+                </div>
 
-            <a href="#" className="arrowUp"><h3>⮝</h3></a>
-        </footer>
-    </div>
-);
+            </footer>
+        </div>
+    );
+};
 
 export default Footer;
